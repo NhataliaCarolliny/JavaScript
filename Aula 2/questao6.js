b19d7aa3f9dbdf788b68aabc2e99e1138c2f9988
Nhatalia Carolliny <nhataliacarolliny@gmail.com>
2025-05-09 22:53:57 -0300

    Add files via upload

---
+++ Aula 2/questao6.js
@@ -0,0 +1,15 @@
+let nome, peso, altura;
+let confirmacao;
+
+nome = prompt("Qual o seu nome? ");
+peso = prompt("Qual o seu peso? ");
+altura = prompt("Qual sua altura? ");
+
+console.log("Seus dados:");
+console.log("Nome: ", nome);
+console.log("Peso: ", peso, "kg");
+console.log("Altura: ", altura, "m");
+
+confirmacao = confirm("Deseja continuar?");
+
+confirmacao = true ? console.log(nome, "seu IMC é", imc) : console.log("Você saiu do programa");
