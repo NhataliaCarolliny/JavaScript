75c5c5e28f134b4cc3e0d0b2a5d2c80fbca662df
Nhatalia Carolliny <nhataliacarolliny@gmail.com>
2025-05-10 09:40:48 -0300

    Atualizar o questao5.js

--- Aula 2/questao5.js
+++ Aula 2/questao5.js
@@ -2,4 +2,4 @@ let conta = true;
 
 conta = confirm("Deseja excluir sua conta?");
 
-conta =  true ? alert("Conta excluída com sucesso!") : alert("Ação cancelada pelo usuário.");
+conta ==  true ? alert("Conta excluída com sucesso!") : alert("Ação cancelada pelo usuário.");
