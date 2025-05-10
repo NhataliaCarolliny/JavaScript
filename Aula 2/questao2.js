b38f27ed0bdfd04d0741ede27375b9c44dda22cc
Nhatalia Carolliny <nhataliacarolliny@gmail.com>
2025-05-09 21:15:53 -0300

    Add files via upload

---
+++ Aula 2/questao2.js
@@ -0,0 +1,12 @@
+let valor;
+
+valor = prompt("Informe o valor da compra: ");
+
+let valorDesconto10 = valor*0.10;
+let valorDesconto5 = valor*0.05;
+
+console.log("Valor original: ", valor);
+
+valor >= 100 ? console.log("Desconto aplicado: ", valorDesconto10) : console.log("Desconto aplicado: ", valorDesconto5);
+
+console.log("Valor total: ", valor);
