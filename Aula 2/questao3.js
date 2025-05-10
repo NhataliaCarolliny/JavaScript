403b702b40b9a73323da77e01dd43303d3b1eff3
Nhatalia Carolliny <nhataliacarolliny@gmail.com>
2025-05-09 22:13:20 -0300

    Update questao3.js

--- Aula 2/questao3.js
+++ Aula 2/questao3.js
@@ -2,7 +2,7 @@ let idade;
 
 idade = prompt("Informe sua idade: ");
 
-let permitido;
+let permitido = true;
 permitido = idade >= 18 ? true : false;
 
 idade >= 18 ? alert("Entrada liberada") : alert("Entrada negada");
