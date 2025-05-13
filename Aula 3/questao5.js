6e09ee1b41bddfa9a52f65dd31ce56a6a6ed0afc
Nhatalia Carolliny <nhataliacarolliny@gmail.com>
2025-05-13 16:28:06 -0300

    Add files via upload

---
+++ Aula 3/questao5.js
@@ -0,0 +1,8 @@
+let n1, n2;
+
+n1 = Number(prompt("Valor inicial: "));
+n2 = Number(prompt("Valor final: "));
+
+for(let contador = n1; contador <= n2; contador++) {
+    console.log(contador, " ");
+}
