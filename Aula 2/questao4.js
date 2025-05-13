696a7aae9b2a238632647000040e67a03dbfc30d
Nhatalia Carolliny <nhataliacarolliny@gmail.com>
2025-05-13 09:48:10 -0300

    Update questao4.js

--- Aula 2/questao4.js
+++ Aula 2/questao4.js
@@ -3,6 +3,9 @@ let estoque, remover;
 estoque = prompt("Quantidade atual no estoque: ");
 remover = prompt("Quantidade que deseja remover: ");
 
+estoque = Number(estoque);
+remover = Number(remover);
+
 let estoqueAtualizado = estoque - remover;
 
 estoqueAtualizado > 0 ? console.log("Estoque atualizado: ", estoqueAtualizado) : console.log("Operação invalida: Quantidade insuficiente no estoque");
