e70e63649da3a78ac4a21d64bec3a2d8bc81d3de
Nhatalia Carolliny <nhataliacarolliny@gmail.com>
2025-05-16 20:57:26 -0300

    Update Projeto.js

--- Projeto - Jogo da advinhação/Projeto.js
+++ Projeto - Jogo da advinhação/Projeto.js
@@ -1,45 +1,51 @@
 let nome = [], numero, tentativas = 1, numeroSorteado, confirmacao, melhorJogador, contador = 0;
 
-alert("Seja bem-vindo ao jogo da advinhação!");
+while (true) {
 
-nome[contador] = prompt("Qual o seu nome, jogador?");
+    alert("Seja bem-vindo ao jogo da advinhação!");
 
-alert(`Olá, ${nome}! \nPreparando o jogo...`);
+    nome[contador] = prompt("Qual o seu nome, jogador?");
 
-alert("Um número de 0 a 100 foi sorteado! \nTente advinhar qual é. Boa Sorte!");
+    alert(`Olá, ${nome}! \nPreparando o jogo...`);
 
-numeroSorteado = parseInt(Math.ceil(Math.random()*100));
+    alert("Um número de 0 a 100 foi sorteado! \nTente advinhar qual é. Boa Sorte!");
 
-while(true) {
+    numeroSorteado = parseInt(Math.ceil(Math.random()*100));
 
-    numero = Number(prompt("Digite um número:"));
+    console.log(numeroSorteado);
+    
+    while(true) {
+        
+        numero = Number(prompt("Digite um número:"));
 
-    if(numero != numeroSorteado) {
-        if(numero < numeroSorteado) {
-            alert("O número que você digitou é MENOR que o número sorteado.");
+        if(numero != numeroSorteado) {
+            if(numero < numeroSorteado) {
+                alert("O número que você digitou é MENOR que o número sorteado.");
+            }
+            else {
+                alert("O número que você digitou é MAIOR que o número sorteado.");
+            }
+            tentativas++
         }
         else {
-            alert("O número que você digitou é MAIOR que o número sorteado.");
+            if(tentativas <= 3) {
+                alert(`Parabéns ${nome}!, você acertou em ${tentativas} tentativa(as). \nUau! Você é um gênio da adivinhação! 🧠`)
+                break
+            }
+            else if(tentativas >= 4 && tentativas <= 6) {
+                alert(`Parabéns ${nome}!, você acertou em ${tentativas} tentativa(as). \nMuito bem! Você foi rápido! 🚀`)
+                break
+            }
+            else {
+                alert(`Parabéns ${nome}!, você acertou em ${tentativas} tentativa(as). \nConseguiu! Persistência é tudo! 💪`)
+                break
+            }  
         }
-        tentativas++
     }
-    else {
-        if(tentativas <= 3) {
-            alert(`Parabéns ${nome}!, você acertou em ${tentativas} tentativa(as). \nUau! Você é um gênio da adivinhação! 🧠`)
-        }
-        else if(tentativas >= 4 && tentativas <= 6) {
-            alert(`Parabéns ${nome}!, você acertou em ${tentativas} tentativa(as). \nMuito bem! Você foi rápido! 🚀`)
-        }
-        else {
-            alert(`Parabéns ${nome}!, você acertou em ${tentativas} tentativa(as). \nConseguiu! Persistência é tudo! 💪`)
-        }  
-
-        confirmacao = confirm("Deseja jogar novamente?");
-
+    confirmacao = confirm("Deseja jogar novamente?");
         if (confirmacao == false) {
             alert(`🏆 Novo recorde! ${nome} é o melhor jogador com ${tentativas} tentativa(as).`);
             break
         }
-    }
     contador++
 }
