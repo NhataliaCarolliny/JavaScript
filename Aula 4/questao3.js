0c7c744c5caa1a01e15a6af20b2b919ac87032ac
Nhatalia Carolliny <nhataliacarolliny@gmail.com>
2025-05-18 17:12:54 -0300

    Add files via upload

---
+++ Aula 4/questao3.js
@@ -0,0 +1,18 @@
+function mensagemDeBoasVindas (nome) {
+    let data = new Date();
+    let hora = data.getHours();
+
+    if (hora >= 6 && hora <= 11){
+        return alert(`Bom dia, ${nome}!`);
+    }
+    else if (hora >= 12 && hora <= 17) {
+        return alert(`Boa tarde, ${nome}!`);
+    }
+    else if (hora >= 18 && hora <= 23) {
+        return alert(`Boa noite, ${nome}!`);
+    }
+}
+
+nome = prompt("Qual o seu nome?");
+
+mensagemDeBoasVindas(nome);
