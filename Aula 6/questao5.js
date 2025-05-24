93cad36de0e34f49eea4ebf41469d3a43ecf535f
Nhatalia Carolliny <nhataliacarolliny@gmail.com>
2025-05-24 10:49:52 -0300

    Update questao5.js

--- Aula 6/questao5.js
+++ Aula 6/questao5.js
@@ -1,7 +1,7 @@
 function protegerObejetos (produto, modoDeProtecao) {
     if (modoDeProtecao === "extensivel") {
         Object.preventExtensions(produto);
-        console.log("Extensivel: ", ture,", Selado: ", false,", Congelado: ", false);
+        console.log("Extensivel: ", true,", Selado: ", false,", Congelado: ", false);
     }
     else if (modoDeProtecao === "selado") {
         Object.seal(produto);
