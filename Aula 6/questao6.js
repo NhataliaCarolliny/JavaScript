c440a6f655be29ca64781666e7b25e7eceb634d7
Nhatalia Carolliny <nhataliacarolliny@gmail.com>
2025-05-23 21:01:59 -0300

    Update questao6.js

--- Aula 6/questao6.js
+++ Aula 6/questao6.js
@@ -1,15 +1,33 @@
-function gerenciarProdutos (produtos, comando) {
+function gerenciarProdutos (produtos, comando, nomeProduto, qntEstoque) {
     if (comando === "listarDisponiveis") {
-        console.log(Object.values(produtos));
+        let listar = produtos.filter(function(produtos) {
+            return produtos.estoque > 0;    
+        })
+        for (let chave in listar) {
+            console.log(listar[chave].nome);
+        }
     }
     else if (comando === "buscarProduto") {
-
+        let buscar = produtos.find(function(buscar) {
+            return buscar.nome === nomeProduto;
+        });
+        console.log(buscar);
     }
     else if (comando === "calcularValorTotal") {
-
+        let soma = 0;
+        for (let chave in produtos) {
+            let valorTotal = produtos[chave].preco*produtos[chave].estoque;
+            soma += valorTotal;
+        }
+        console.log(`Valor total do estoque: ${soma}`);
     }
     else if (comando === "atualizarEstoque") {
-
+        for (let chave in produtos) {
+            if (produtos[chave].nome === nomeProduto) {
+                produtos[chave].estoque = qntEstoque;
+                console.log(`ESTOQUE ATUALIZADO: \n${produtos[chave].nome} => Estoque: ${produtos[chave].estoque}`);
+            }   
+        }
     }
 }
 
@@ -19,4 +37,18 @@ const produtos = [
     {nome: "Fone de ouvido", preco: 250, estoque: 12}
 ];
 
-gerenciarProdutos (produtos, "listarDisponiveis");
+// Listar disponíveis
+gerenciarProdutos(produtos, "listarDisponiveis");
+console.log("\n");
+
+// Buscar um produto
+gerenciarProdutos(produtos, "buscarProduto", "Notebook");
+console.log("\n");
+
+// Calcular valor total
+gerenciarProdutos(produtos, "calcularValorTotal");
+console.log("\n");
+
+// Atualizar estoque
+gerenciarProdutos(produtos, "atualizarEstoque", "Notebook", 3);
+console.log("\n");
