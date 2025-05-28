ad0fb21c8eb55b7c1733d1a4e6a9d2ae798ef997
Nhatalia Carolliny <nhataliacarolliny@gmail.com>
2025-05-27 21:35:35 -0300

    Update questao4.js

--- Aula 5/questao4.js
+++ Aula 5/questao4.js
@@ -1,8 +1,8 @@
 function obterMaior (numeros) {
     let maiorNumero = 0;
-    for (maiorNumero of numeros) {
-        if(numeros > maiorNumero) {
-            maiorNumero = numeros;
+    for (chave of numeros) {
+        if(chave > maiorNumero) {
+            maiorNumero = chave;
         } 
     }    
     console.log(`Maior número: ${maiorNumero}`);
@@ -11,8 +11,8 @@ function obterMaior (numeros) {
 function obterMenor (numeros) {
     let menorNumero = 0;
     for (let chave of numeros) {
-        if(numeros[chave] < menorNumero) {
-            menorNumero = numeros[chave];
+        if(chave < menorNumero) {
+            menorNumero = chave;
         } 
     }    
     console.log(`Menor número: ${menorNumero}`);
