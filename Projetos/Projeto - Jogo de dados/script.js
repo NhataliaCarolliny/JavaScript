24fe330754a367478f61adb1b6a1441ce6f3f4d4
Nhatalia Carolliny <nhataliacarolliny@gmail.com>
2025-05-31 20:38:56 -0300

    Add files via upload

---
+++ Projetos/Projeto - Jogo de dados/script.js
@@ -0,0 +1,6 @@
+const atualizar = document.querySelector(".container");
+const jogador1 = document.querySelectorAll(".img1");
+const jogador2 = document.querySelectorAll(".img2");
+
+jogador1 = parseInt(Math.ceil(Math.random()*6));
+jogador2 = parseInt(Math.ceil(Math.random()*6));
